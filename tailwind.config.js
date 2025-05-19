fe200460b347714c7b27ffb263f18a349f9c6ffb
kostasmath23 <kostasmath23@gmail.com>
2025-05-19 03:43:25 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
       fontFamily: {
         serif: ["'Cormorant Garamond'", "serif"],
         sans: ["'Open Sans'", "sans-serif"]
-      }
+      },
+      animation: {
+        'floating-slow': 'float 8s ease-in-out infinite',
+        'floating-medium': 'float 6s ease-in-out infinite',
+        'floating-fast': 'float 4s ease-in-out infinite',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-15px)' },
+        },
+      },
     }
   },
   plugins: [],
